4cdeb44ae22d4e5ace8cf6f728eddba06a823c2b
Chad Lengefeld <chadlengefeld@gmail.com>
2020-03-30 07:41:29 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,10 +3,10 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
 
-      get '/names' do
+      get '/name' do
       "My name is Chad"
       end
-  
+
 
   # get '/' do
   #   "hometown"
